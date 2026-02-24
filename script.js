40dcb43bd6980808e7829fc914d0ce416eee4491
unknown <rakibulhasanridoy1@gmail.com>
2026-02-24 18:47:43 +0600

    Button Toggle complete

--- script.js
+++ script.js
@@ -18,3 +18,38 @@ function calculateCount() {
     rejectedCount.innerText = rejectedList.length;
 }
 calculateCount();
+function toggleStyle(id) {
+    
+
+    [allFilterBtn, interviewFilterBtn, rejectedFilterBtn].forEach(btn => {
+    btn.classList.remove('bg-blue-500', 'text-white');
+    btn.classList.add('bg-white', 'text-gray-600', 'border', 'border-gray-200');
+    });
+    allFilterBtn.classList.remove('bg-blue-500', 'text-white');
+    interviewFilterBtn.classList.remove('bg-blue-500', 'text-white');
+    rejectedFilterBtn.classList.remove('bg-blue-500', 'text-white');
+
+
+
+    const selected = document.getElementById(id);
+    currentStatus = id;
+
+
+    
+selected.classList.remove('bg-white', 'text-gray-600', 'border', 'border-gray-200');
+selected.classList.add('bg-blue-500', 'text-white');
+
+    
+if (id == 'interview-filter-btn') {
+     allCardSection.classList.add('hidden');
+      filterSection.classList.remove('hidden');
+      renderInterview();
+} else if (id == 'all-filter-btn') {
+     allCardSection.classList.remove('hidden');
+     filterSection.classList.add('hidden');
+} else if (id == 'rejected-filter-btn') {
+     allCardSection.classList.add('hidden');
+     filterSection.classList.remove('hidden');
+     renderRejected();
+    }
+}
